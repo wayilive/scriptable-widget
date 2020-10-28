647cba65941ca46b080272c35b1098f8d869d70e
wayilive <closetoyou@gmail.com>
2020-10-28 18:04:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange icon-glyph: quote-right
 
-const User = 'USER'
-const City = 'beijing'
-const WeatherKey = '' // you can get it from https://dev.heweather.com/
-const AQIToken = '' // you can get it from https://aqicn.org/data-platform/token/#/
+const User = 'Bill'
+//const City = 'beijing'
+//const WeatherKey = '' // you can get it from https://dev.heweather.com/
+//const AQIToken = '' // you can get it from https://aqicn.org/data-platform/token/#/
 
-const aqi = await getAQI()
+//const aqi = await getAQI()
 const lunarData = await getLunarData()
-const weatherData = await getWeather()
+//const weatherData = await getWeather()
 
 const widget = createWidget()
 Script.setWidget(widget)
@@ -49,9 +49,9 @@ function createWidget() {
     Line2.textColor = new Color('#C6FFDD')
     Line2.font = new Font('Menlo', 11)
 
-    const Line3 = w.addText(`[☁️]${weatherData} AQI:${aqi}`)
-    Line3.textColor = new Color('#FBD786')
-    Line3.font = new Font('Menlo', 11)
+    //const Line3 = w.addText(`[☁️]${weatherData} AQI:${aqi}`)
+    //Line3.textColor = new Color('#FBD786')
+    //Line3.font = new Font('Menlo', 11)
 
     const Line4 = w.addText(`[${Device.isCharging() ? '⚡️' : '🔋'}]${renderBattery()} Battery`)
     Line4.textColor = new Color('#2aa876')
@@ -63,21 +63,21 @@ function createWidget() {
 
     return w
 }
-
+/*
 async function getAQI() {
     const url = `https://api.waqi.info/feed/${City}/?token=${AQIToken}`
     const request = new Request(url)
     const res = await request.loadJSON()
     return res.data.aqi
 }
-
+*/
 async function getLunarData() {
     const url = 'https://api.xlongwei.com/service/datetime/convert.json'
     const request = new Request(url)
     const res = await request.loadJSON()
     return `${res.ganzhi}年（${res.shengxiao}）${res.chinese.replace(/.*年/, '')}`
 }
-
+/*
 async function getWeather() {
     const requestCityInfo = new Request(
         `https://geoapi.heweather.net/v2/city/lookup?key=${WeatherKey}&location=${City}&lang=en`
@@ -92,7 +92,7 @@ async function getWeather() {
 
     return `${name} ${resNow.now.text} T:${resNow.now.temp}° H:${resDaily.daily[0].tempMax}° L:${resDaily.daily[0].tempMin}°`
 }
-
+*/
 function renderProgress(progress) {
     const used = '▓'.repeat(Math.floor(progress * 24))
     const left = '░'.repeat(24 - used.length)
